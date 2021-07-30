59884393f24c2496dffb8ed0c87301d26f4e585e
Basilio Farach <basiliofarach@pop-os.localdomain>
2021-07-30 02:24:09 -0600

    Changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing changed!
+          Feature branch change!
         </a>
       </header>
     </div>
