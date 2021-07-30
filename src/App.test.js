2407185c4da6530977bae22adf8944b0ac255020
Basilio Farach <basiliofarach@pop-os.localdomain>
2021-07-30 02:44:27 -0600

    Added matching test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Testing changed!/i);
+  const linkElement = screen.getByText(/Testing changed v2!/i);
   expect(linkElement).toBeInTheDocument();
 });
